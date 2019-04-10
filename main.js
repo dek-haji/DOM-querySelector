eed5fe54d9212694b2026273879dbcf23059632b
dek-haji <deeeqhaji@gmail.com>
2019-04-10 14:18:31 -0500

    assignment done

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector(".article__header").textContent = "Welcome to Dek's blog";
+document.querySelector(".article__header").classList.add("important");
+document.querySelector(".aside_box--dark").classList.remove("dashed");
+document.querySelector(".article__footer").classList.add("goldenrod")
